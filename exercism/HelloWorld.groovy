82b3525bf2d80f297dbd95cf323ae73d26687fd0
marekstepan <marek.stepan@gmail.com>
2021-05-04 21:53:54 +0200

    adding exercism helloworld

---
+++ exercism/HelloWorld.groovy
@@ -0,0 +1,13 @@
+// This is a skeleton file for the Groovy "Hello World" exercise.
+
+class HelloWorld {
+
+    static void main(String[] args) {
+      hello()
+    }
+
+    static def hello() {
+        println("Hello, World!")
+    }
+
+}
